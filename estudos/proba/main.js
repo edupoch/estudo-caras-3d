8fbe2d34fa511eeb5f2645a31ed812d18b78a5c6
edupoch <edupoch@gmail.com>
2025-02-19 20:46:47 +0100

    Pintamos a cara tamén con puntos

--- estudos/proba/main.js
+++ estudos/proba/main.js
@@ -1,15 +1,19 @@
 import * as THREE from "three";
 
+import { uniform, skinning } from "three/tsl";
+
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import * as BufferGeometryUtils from "three/addons/utils/BufferGeometryUtils.js";
+import { VertexTangentsHelper } from "three/addons/helpers/VertexTangentsHelper.js";
 import { GUI } from "dat.gui";
 
 // Modelo de cara
 import modeloGLB from "../../modelos/First_Face_Scan.glb";
 
 let camera, scene, renderer;
+let modelo;
 let conWireframe = false;
-let conTextura = true;
 
 init();
 
@@ -23,23 +27,56 @@ function init() {
     0.25,
     20
   );
-  camera.position.set(0, 0, 1.5);
+  camera.position.set(0, 0, 3.5);
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xffffff);
+  // scene.background = new THREE.Color(0x000000);
 
   const loader = new GLTFLoader();
   loader.load(modeloGLB, function (gltf) {
-    const modelo = gltf.scene;
-    modelo.traverse((o) => {
-      if (o.isMesh) {
-        o.material.metalness = !conTextura;
-        o.material.wireframe = conWireframe;
+    modelo = gltf.scene;
+    modelo.traverse((child) => {
+      if (child.isMesh) {
+        child.material.wireframe = conWireframe;
       }
     });
+
     modelo.scale.set(5, 5, 5);
     modelo.rotation.y = Math.PI / 2.5;
+    modelo.position.x = 0;
     scene.add(modelo);
+
+    const mesh = modelo.children[0];
+
+    const wireframe = new THREE.WireframeGeometry(mesh.geometry);
+    const lineMaterial = new THREE.LineBasicMaterial({
+      color: 0x000000,
+      linewidth: 1,
+    });
+    let line = new THREE.LineSegments(wireframe, lineMaterial);
+    line.scale.set(5, 5, 5);
+    line.rotation.y = Math.PI / 2.5;
+    line.position.x = 2;
+    scene.add(line);
+
+    let meshGeometry = mesh.geometry.clone();
+    const positions = meshGeometry.attributes.position.array;
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute(positions, 3)
+    );
+    const material = new THREE.PointsMaterial({ size: 0.01, color: 0x000000 });
+
+    const pointCloud = new THREE.Points(geometry, material);
+
+    pointCloud.scale.set(5, 5, 5);
+    pointCloud.rotation.y = Math.PI / 2.5;
+    pointCloud.position.x = -2.2;
+
+    scene.add(pointCloud);
   });
 
   let ambientLight = new THREE.AmbientLight(0x323232, 100);
@@ -48,8 +85,8 @@ function init() {
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
-  //   renderer.toneMapping = THREE.ACESFilmicToneMapping;
-  //   renderer.toneMappingExposure = 1;
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 1;
   renderer.setAnimationLoop(animate);
   container.appendChild(renderer.domElement);
 
@@ -66,20 +103,9 @@ function init() {
     .name("Con wireframe")
     .onChange(function (value) {
       conWireframe = value;
-      scene.traverse(function (child) {
+      modelo.traverse(function (child) {
         if (child.isMesh) {
           child.material.wireframe = value;
-        }
-      });
-    });
-  gui
-    .add({ metalness: conTextura }, "metalness")
-    .name("Con textura")
-    .onChange(function (value) {
-      conTextura = !value;
-      scene.traverse(function (child) {
-        if (child.isMesh) {
-          child.material.metalness = conTextura;
         }
       });
     });
