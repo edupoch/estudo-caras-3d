b0bac0e8210906095002c13f526fe5870d133bf3
edupoch <edupoch@gmail.com>
2025-03-07 12:20:24 +0100

    Proba de postprocessing con detección de bordes

--- estudos/efectos/main.js
+++ estudos/efectos/main.js
@@ -6,8 +6,11 @@ import { GUI } from "dat.gui";
 
 import { EffectComposer } from "three/examples/jsm/Addons.js";
 import { RenderPass } from "three/examples/jsm/Addons.js";
+import { ShaderPass } from "three/addons/postprocessing/ShaderPass.js";
 import { HalftonePass } from "three/examples/jsm/Addons.js";
 import { OutputPass } from "three/examples/jsm/Addons.js";
+import { LuminosityShader } from "three/addons/shaders/LuminosityShader.js";
+import { SobelOperatorShader } from "three/addons/shaders/SobelOperatorShader.js";
 
 // Modelo de cara
 import modeloGLB from "../../modelos/First_Face_Scan.glb";
@@ -16,6 +19,7 @@ let camera, scene, renderer, composer;
 let modelo;
 
 let halftonePassEnabled = true;
+let sobelEnabled = false;
 
 const ESCALA = 5;
 const ROTACION = Math.PI / 2.5;
@@ -55,20 +59,41 @@ function init() {
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   // Melloramos o tono da foto
-  renderer.toneMapping = THREE.ACESFilmicToneMapping;
-  renderer.toneMappingExposure = 1;
+  // renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  // renderer.toneMappingExposure = 1;
 
   renderer.setAnimationLoop(animate);
   container.appendChild(renderer.domElement);
 
   composer = new EffectComposer(renderer);
   composer.addPass(new RenderPass(scene, camera));
+
   const halftonePass = new HalftonePass(window.innerWidth, window.innerHeight, {
     radius: 10,
     greyscale: true,
   });
   halftonePass.enabled = halftonePassEnabled;
   composer.addPass(halftonePass);
+
+  const sobelShader = SobelOperatorShader;
+  // Coloreamos os bordes en negro sobre fondo branco
+  sobelShader.fragmentShader = sobelShader.fragmentShader.replace(
+    "gl_FragColor = vec4( vec3( G ), 1 );",
+    `if (G > 0.8)
+      gl_FragColor = vec4(0, 0, 0, 1);
+    else
+      gl_FragColor = vec4(1, 1, 1, 1);`
+  );
+
+  const effectSobel = new ShaderPass(sobelShader);
+  effectSobel.uniforms["resolution"].value.x =
+    window.innerWidth * window.devicePixelRatio;
+  effectSobel.uniforms["resolution"].value.y =
+    window.innerHeight * window.devicePixelRatio;
+
+  effectSobel.enabled = sobelEnabled;
+  composer.addPass(effectSobel);
+
   composer.addPass(new OutputPass());
 
   const controls = new OrbitControls(camera, renderer.domElement);
@@ -86,6 +111,15 @@ function init() {
       halftonePassEnabled = value;
       composer.passes[1].enabled =
         composer.passes[1].enabled === true ? false : true;
+    });
+
+  gui
+    .add({ sobelEnabled: sobelEnabled }, "sobelEnabled")
+    .name("Bordes")
+    .onChange(function (value) {
+      sobelEnabled = value;
+      composer.passes[2].enabled =
+        composer.passes[2].enabled === true ? false : true;
     });
 
   window.addEventListener("resize", onWindowResize);
